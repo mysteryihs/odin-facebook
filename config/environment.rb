213d10606ebbca46cb45aad32cb2bfa7c9360c6c
Oscar Cheng Yih <oscaryih@hotmail.com>
2016-12-13 23:31:00 -0800

    Add environment.rb setup for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app60655461@heroku.com',
+  :password => 'erf8fbys9417',
+  :domain => 'http://guarded-bastion-44854.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
